15aefc3be93c651042f5b6356513b2191499e877
Shoaib Khan <147058748+shoaib0904@users.noreply.github.com>
2024-04-18 18:43:43 +0530

    Update app.py

--- main/app.py
+++ main/app.py
@@ -56,4 +56,4 @@ def watermark_image():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5000)
